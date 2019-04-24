077b35030269e95b5b24a8ea3bffec0e95829ab4
hodish1 <hodaia43@gmail.com>
2019-04-24 21:49:03 +0300

    bottom partials

---
+++ partials/btm-menu.php
@@ -0,0 +1,13 @@
+<div class="btm-menu-wrapper">
+    <div class="darken">
+
+        <div class="btm-plugins">
+            מתישהו יהיו פה פלאגינים של רשתות חברתיות
+        </div>
+
+        <div class="btm-links">
+            
+        </div>
+    
+    </div>
+</div>
